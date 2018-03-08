998c08903448416ed7dc02e4eb21820b64d618fb
Taras Sava <sava.taras@gmail.com>
2018-03-08 16:17:15 +0000

    AddMobScroll

--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
-$(".carousel").swipe({
-
-  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-    if (direction == 'left') $(this).carousel('next');
-    if (direction == 'right') $(this).carousel('prev');
-
-  },
-  allowPageScroll:"vertical"
-
-});
+$("#carousel-example-generic").carousel();
