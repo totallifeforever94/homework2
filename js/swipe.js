af23b209261e5c6893681e2d97452a36db0e5e21
Taras Sava <sava.taras@gmail.com>
2018-03-11 13:50:42 +0000

    Swipe

--- js/swipe.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-   $("#swipe").swiperight(function() {
-      $(this).carousel('prev');
-    });
-   $("#swipe").swipeleft(function() {
-      $(this).carousel('next');
-   });
-});
